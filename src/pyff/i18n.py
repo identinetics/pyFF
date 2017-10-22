6ed5ab056391b91c22879c2528903aa688913a9c
r2h2 <rainer@hoerbe.at>
2017-10-22 23:26:37 +0200

    add log msg for i18n

--- src/pyff/i18n.py
+++ src/pyff/i18n.py
@@ -1,9 +1,13 @@
+from __future__ import print_function
 __author__ = 'leifj'
 # -*- coding: utf-8 -*-
 
 import os
 import gettext
+import logging
 
 LOCALE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "i18n")
 
 language = gettext.translation('messages', LOCALE_DIR, fallback=True)
+
+logging.debug('Locale: ' + language._info['language'], ', rev: ' + language._info['po-revision-date'])
